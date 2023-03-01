2ff736dc78f0adfb1c0e7d010e77dc01563c0a83
storialp <luis.pericchi@studbocconi.it>
2023-03-01 21:02:41 +0100

    Fixed heroicons deployment issue (needed to add index.js to path)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline.js'
+import {
+  Bars3Icon,
+  BellIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline/index.js'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
