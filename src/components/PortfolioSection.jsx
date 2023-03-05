9d128ca8e9677371748d4ed91b802108dc7d19ba
storialp <luis.pericchi@studbocconi.it>
2023-03-05 14:06:33 +0100

    Fixed svg issue and troubleshooted navbar conditional styling

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -117,7 +117,7 @@ const people = [
                 y2='71.8295'
                 gradientUnits='userSpaceOnUse'
               >
-                <stop stop-color='#249361' />
+                <stop stopColor='#249361' />
                 <stop offset='1' stop-color='#3ECF8E' />
               </linearGradient>
               <linearGradient
@@ -129,7 +129,7 @@ const people = [
                 gradientUnits='userSpaceOnUse'
               >
                 <stop />
-                <stop offset='1' stop-opacity='0' />
+                <stop offset='1' stopOpacity='0' />
               </linearGradient>
             </defs>
           </svg>
