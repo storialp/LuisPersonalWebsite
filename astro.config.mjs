af83d72458a79b146c495b59053e7598724579e1
storialp <luis.pericchi@studbocconi.it>
2026-03-14 13:32:09 +0100

    Added Astro Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// https://astro.build/config
 import react from '@astrojs/react';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [tailwind(), react()],
 });
