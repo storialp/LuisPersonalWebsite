26b3105e16eb882759c88b46ed8cd076806d4b29
storialp <luis.pericchi@studbocconi.it>
2026-03-14 14:42:33 +0100

    Fixed outdated rendering mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: 'hybrid',
+  output: "static",
   adapter: vercel(),
   integrations: [tailwind(), react()],
 });
