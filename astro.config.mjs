9a419d189d95b723ae574427a59fc606920453d9
storialp <luis.pericchi@studbocconi.it>
2026-03-19 13:08:39 +0100

    Moved away from the tailwind astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [react()],
 });
